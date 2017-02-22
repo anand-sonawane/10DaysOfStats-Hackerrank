6a8030875f26a2b64f7a54335dc1492fab06d7cd
anand-sonawane <avsonawane28@gmail.com>
2017-02-23 00:39:07 +0530

    Update Mean,Median and Mode.py

--- Day0/Mean,Median and Mode.py
+++ Day0/Mean,Median and Mode.py
@@ -1,1 +1,26 @@
+N=int(input())
+a=[]
 
+a=list(map(int,input().split()))
+
+sum=0
+for i in range(N):
+    sum=sum+a[i]
+mean=round(sum/N,1)
+print(mean)
+
+a.sort()
+if(N%2==0):
+    median=round((a[int(N/2)]+a[int(N/2-1)])/2,1)
+else:
+    median=round(a[int(N/2)])
+print(median)
+
+count_a={}
+max_crr=0
+for i in range(N):
+    max=(a.count(a[i]))
+    if(max>max_crr):
+        mode=a[i]
+        max_crr=max
+print(mode)
